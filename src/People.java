7c43f76996268479bfcb572a2a8bb925e54af64b
sd <s_d_a@mail.ru>
2019-01-25 19:19:04 +0300

    commit some new Class

---
+++ src/People.java
@@ -0,0 +1,35 @@
+public class People {
+    private String name;
+    private Integer age;
+    private Sex sex;
+
+    public People(String name, int age, Sex sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+}
--- src/StreamExp.java
+++ src/StreamExp.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamExp {
     public static void main(String[] args) {
@@ -32,5 +35,61 @@ public class StreamExp {
                 .filter(i -> i % 2 == 0)
                 .sorted()
                 .forEachOrdered(System.out::println);
+
+        streamExp();
+
+    }
+
+    public static void streamExp(){
+        Stream<String> streamFromValues = Stream.of("a1", "a2", "a3");
+        streamFromValues.forEach(System.out::println);
+
+        List<String> list = new ArrayList();
+        list.add("a1");
+        list.add("a2");
+        list.add("a3");
+        list.add("a4");
+
+        list.stream().map(s -> s + "_1");
+        List<String> newList =  list.stream().map(s -> s + "_1").collect(Collectors.toList());
+        newList.forEach(System.out::println);
+
+        List<People> peoples = Arrays.asList(
+                new People("Вася", 16, Sex.MAN),
+                new People("Петя", 23, Sex.MAN),
+                new People("Елена", 42, Sex.WOMAN),
+                new People("Иван Иванович", 69, Sex.MAN));
+
+        // Отбор
+        List<People> sortedPeople = peoples.stream().filter( people ->
+                people.getAge() >= 18 && people.getAge() < 27 && people.getSex() == Sex.MAN)
+                .collect(Collectors.toList());
+        sortedPeople.forEach(p -> System.out.println(p.getName()));
+
+        // Средний возраст
+        System.out.println(
+        peoples.stream().filter((p) -> p.getSex() == Sex.MAN).
+                mapToInt(People::getAge).average().getAsDouble()
+        );
+
+        // Двойной фильтр
+        System.out.println(
+        peoples.stream().filter((p) -> p.getAge() >= 18).filter(
+                (p) -> (p.getSex() == Sex.WOMAN && p.getAge() < 55) || (p.getSex() == Sex.MAN && p.getAge() < 60)).count()
+        );
+
+        System.out.println(
+          peoples.stream().anyMatch(people -> people.getName().equals("Петя"))
+        );
+
+        // Сортировка по имени в обратном порядке
+        peoples.stream().sorted((o1, o2) -> - o1.getName().compareTo(o2.getName())).collect(Collectors.toList())
+                .forEach( people -> System.out.println(people.getName()));
+
+        // Сортировка по двум параметрам
+
+        peoples.stream().sorted((o1, o2) -> o1.getSex() != o2.getSex()? o1.getSex().
+                compareTo(o2.getSex()): o1.getAge().compareTo(o2.getAge())).collect(Collectors.toList())
+                .forEach(people -> System.out.println(people.getName()));
     }
 }
